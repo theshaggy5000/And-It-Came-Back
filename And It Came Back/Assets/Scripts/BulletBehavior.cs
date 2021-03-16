ddf1396dfdfd974be7212929fb82936aa5fcb685
Vyryty <colonelsuperawesome@gmail.com>
2021-03-16 15:59:05 -0500

    Added Bullet Prefab And Behavior Script

--- And It Came Back/Assets/Scripts/BulletBehavior.cs
+++ And It Came Back/Assets/Scripts/BulletBehavior.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [HideInInspector]public Rigidbody2D rb2d;
+    public float speed = 10f;
+
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.velocity = new Vector2(1, 1) * speed;
+    }
+
+
+
+    void Awake()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    
+
+    void FixedUpdate()
     {
-        
+        Debug.Log(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        Debug.Log((bool)Physics2D.Raycast(transform.position, rb2d.velocity, speed, Physics2D.GetLayerCollisionMask(gameObject.layer)));
+        Debug.DrawRay(transform.position, rb2d.velocity, Color.cyan);
     }
 }
