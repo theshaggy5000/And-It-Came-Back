30618e07e875102595ce2ac11df0e4717a100dd0
Vyryty <colonelsuperawesome@gmail.com>
2021-03-23 16:25:18 -0500

    Finished Bullet Behavior

--- And It Came Back/Assets/Scripts/BulletBehavior.cs
+++ And It Came Back/Assets/Scripts/BulletBehavior.cs
@@ -6,27 +6,11 @@ public class BulletBehavior : MonoBehaviour
 {
     [HideInInspector]public Rigidbody2D rb2d;
     public float speed = 10f;
-
+    public Vector2 initialDirection = new Vector2(1, 1);
 
     private void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
-        rb2d.velocity = new Vector2(1, 1) * speed;
-    }
-
-
-
-    void Awake()
-    {
-        
-    }
-
-    
-
-    void FixedUpdate()
-    {
-        Debug.Log(Physics2D.GetLayerCollisionMask(gameObject.layer));
-        Debug.Log((bool)Physics2D.Raycast(transform.position, rb2d.velocity, speed, Physics2D.GetLayerCollisionMask(gameObject.layer)));
-        Debug.DrawRay(transform.position, rb2d.velocity, Color.cyan);
+        rb2d.velocity = initialDirection.normalized * speed;
     }
 }
