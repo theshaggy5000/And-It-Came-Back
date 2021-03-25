1dd9cd2163c6b40b9dd69d46a46cb63eb707acf9
LucasEwald <42420052+LucasEwald@users.noreply.github.com>
2021-03-25 11:27:23 -0500

    Update BulletBehavior.cs
    Added a change speed function to the bullet

--- And It Came Back/Assets/Scripts/BulletBehavior.cs
+++ And It Came Back/Assets/Scripts/BulletBehavior.cs
@@ -4,9 +4,20 @@ using UnityEngine;
 
 public class BulletBehavior : MonoBehaviour
 {
+    Rigidbody2D rb2d;
     public bool infiniteBounces = true; //Whether the bullet should bounce infinitely or not
     public int bounceCount = 0;         //The number of walls the bullet has bounced off of
     public int maxBounces = 5;          //The max number of bounces of a bullet
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void changeSpeed(float speed)
+    {
+        rb2d.velocity = rb2d.velocity.normalized * speed;
+    }
 
     void OnCollisionEnter2D(Collision2D other)
     {
